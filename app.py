701c7a7b06152c5ff1c16e3cf3052b067fdea7f6
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-08-05 15:28:22 +0200

    Formatted code

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-#app.py
+# app.py
 
 my_name = "Tanimara"
 print("Hello and welcome " + my_name + "!")
 
-#this is how to write a comment
+# this is how to write a comment
+
+print("*" * 10)
+
+print(2 + 3)
+
+x = 1
+
+print(len(my_name))
