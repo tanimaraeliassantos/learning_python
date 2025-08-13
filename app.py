9a2e6e136c5cb0a3f1502fc9396b342a6aea4dbd
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-08-13 16:34:12 +0200

    Created greet function

--- app.py
+++ app.py
@@ -229,9 +229,17 @@
 
 # Display even numbers from 1 to 10 and after print message
 # we have four even numbers
-count = 0
-for number in range(1,10):
-    if number %2 == 0:
-        count += 1
-        print(number)
-print(f"We have {count} even numbers")
+# count = 0
+# for number in range(1,10):
+#     if number %2 == 0:
+#         count += 1
+#         print(number)
+# print(f"We have {count} even numbers")
+
+# Functions
+def greet():
+    print("hi there")
+    print("Welcome")
+
+
+greet()
